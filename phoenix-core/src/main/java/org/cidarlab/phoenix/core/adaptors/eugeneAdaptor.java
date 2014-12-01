30c1536995df5a9daa6c250868f5f70334b9e2d9
Evan Appleton <eapple@bu.edu>
2014-12-01 18:43:46 -0500

    added adaptor classes for Phoenix to other apps

---
+++ phoenix-core/src/main/java/org/cidarlab/phoenix/core/adaptors/eugeneAdaptor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cidarlab.phoenix.core.adaptors;
+
+/**
+ * This class has all methods for sending and receiving information to Eugene and miniEugene
+ * 
+ * @author evanappleton
+ */
+public class eugeneAdaptor {
+    
+}
---
+++ phoenix-core/src/main/java/org/cidarlab/phoenix/core/adaptors/ravenAdaptor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cidarlab.phoenix.core.adaptors;
+
+/**
+ * This class has all methods for sending and receiving information to Raven
+ * 
+ * @author evanappleton
+ */
+public class ravenAdaptor {
+    
+}

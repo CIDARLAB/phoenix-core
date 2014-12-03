7786e09b8f9b6b6ab647864d68d367fd6582e2c5
Evan Appleton <eapple@bu.edu>
2014-12-02 20:55:17 -0500

    added Experiment objects to the DOM. Only placeholders right now

---
+++ phoenix-core/src/main/java/org/cidarlab/phoenix/core/dom/Experiment.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cidarlab.phoenix.core.dom;
+
+/**
+ * This file is for the Experiment object
+ * 
+ * @author evanappleton
+ */
+public class Experiment {
+    
+}

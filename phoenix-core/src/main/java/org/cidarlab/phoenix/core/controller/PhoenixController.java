def50f84d2eb1d4ecbc395ff5cf8bcfe651c0cb7
Evan Appleton <eapple@bu.edu>
2014-12-02 20:48:13 -0500

    framework for phoenix controller (i.e. workflow manager) added

---
+++ phoenix-core/src/main/java/org/cidarlab/phoenix/core/controller/PhoenixController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cidarlab.phoenix.core.controller;
+
+/**
+ * This is the primary class for managing the workflow of tools within Phoenix
+ * 
+ * @author evanappleton
+ */
+public class PhoenixController {
+    
+}
--- phoenix-core/src/test/java/org/cidarlab/phoenix/demo/GrammarDemo.java
+++ phoenix-core/src/test/java/org/cidarlab/phoenix/demo/GrammarDemo.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
+ * This file should evolve into the core Phoenix decomposition grammar
  * 
  * @author Ernst Oberortner
  */

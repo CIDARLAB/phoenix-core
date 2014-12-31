c4c16777ed3321da90707fc6dead943ed68a6283
Prashant Vaidyanathan <prash@bu.edu>
2014-12-31 13:22:03 -0500

    Modified module class. Getters and Setter added. Basic constructors added

--- phoenix-core/src/main/java/org/cidarlab/phoenix/core/dom/Module.java
+++ phoenix-core/src/main/java/org/cidarlab/phoenix/core/dom/Module.java
@@ -5,7 +5,12 @@
  */
 package org.cidarlab.phoenix.core.dom;
 
+import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.clothocad.model.Feature;
 
 /**
  *
@@ -13,10 +18,73 @@ import java.util.List;
  */
 public class Module {
     //Need to formally define these methods and constructors. But this is the basic essence of this Class. 
-    LTLFunction function;
-    List<Module> parents;
-    List<Module> children;
-    int stage;
-    Experiment experiment;
-    Interaction interaction;
+    
+    @Getter
+    @Setter
+    private Feature moduleFeature;
+    
+    @Getter
+    @Setter
+    private LTLFunction function;
+    
+    @Getter
+    @Setter
+    private List<Module> parents;
+    
+    @Getter
+    @Setter
+    private List<Module> children;
+    
+    @Getter
+    @Setter
+    private int stage;
+    
+    @Getter
+    @Setter
+    private boolean isRoot;
+    
+    @Getter
+    @Setter
+    private Experiment experiment;
+    
+    @Getter
+    @Setter
+    private Interaction interaction;
+    
+    
+    
+    public Module()
+    {
+        this.stage = -1;
+        this.isRoot = false;
+        this.children = new ArrayList<Module>();
+        this.parents = new ArrayList<Module>();
+        
+    }
+    
+    public Module(boolean _isRoot,int _stage)
+    {
+        this.stage = _stage;
+        this.isRoot = _isRoot;
+        this.children = new ArrayList<Module>();
+        this.parents = new ArrayList<Module>();
+        
+    }
+    
+    public List<Module> getChildren()
+    {
+        return this.children;
+    }
+    public List<Module> getParents()
+    {
+        return this.parents;
+    }
+    public List<Module> getAllNeighbors()
+    {
+        List<Module> neigh = new ArrayList<Module>();
+        neigh.addAll(this.parents);
+        neigh.addAll(this.children);
+        return neigh;
+    }
+    
 }

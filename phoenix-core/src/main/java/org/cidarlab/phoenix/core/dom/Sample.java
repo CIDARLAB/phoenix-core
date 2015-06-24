0f0b3a8641ff7205ba4fe2a3087bfa338a602f02
Evan Appleton <eapple@bu.edu>
2015-06-24 10:30:15 -0400

    sample duplication fixed

--- phoenix-core/src/main/java/org/cidarlab/phoenix/core/dom/Sample.java
+++ phoenix-core/src/main/java/org/cidarlab/phoenix/core/dom/Sample.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang.builder.EqualsBuilder;
 
 /**
  *
@@ -16,15 +17,61 @@ import lombok.Setter;
 public class Sample {
    
     //Constructor
-    public Sample(String _name, SampleType _type, Strain _strain, List<Polynucleotide> _polynucleotides, Medium _media, Integer _time) {
-        name = _name;
-        clothoID = _name;
+    public Sample(SampleType _type, Strain _strain, List<Polynucleotide> _polynucleotides, Medium _media, Integer _time) {
+//        name = _name;
+//        clothoID = _name;
         type = _type;
         strain = _strain;
         polynucleotides = _polynucleotides;
         media = _media;
         time = _time;
+        
+        String pnNames = "";
+        if (_polynucleotides != null) {
+            for (Polynucleotide p : _polynucleotides) {
+                if (pnNames.isEmpty()) {
+                    pnNames = p.getClothoID();
+                } else {
+                    pnNames = pnNames + "_" + p.getClothoID();
+                }
+            }
+        }
+        
+        if (_strain != null && _media != null) {
+            if (pnNames.isEmpty()) {
+                clothoID = _type.toString() + "_" + _strain.getName() + "_" + _media.getName() + "_" + _time;
+            } else {
+                clothoID = pnNames + "_" + _type.toString() + "_" + _strain.getName() + "_" + _media.getName() + "_" + _time;
+            }
+        } else {
+            clothoID = _type.toString();
+        }
     }
+    
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (obj == null) {
+//            return false;
+//        }
+//        if (obj == this) {
+//            return true;
+//        }
+//        if (obj.getClass() != getClass()) {
+//            return false;
+//        }
+//        Sample rhs = (Sample) obj;
+//        return new EqualsBuilder()
+//                .appendSuper(super.equals(obj))
+//                .append(name, rhs.name)
+//                .append(clothoID, rhs.clothoID)
+//                .append(media, rhs.media)
+//                .append(polynucleotides, rhs.polynucleotides)
+//                .append(strain, rhs.strain)
+//                .append(time, rhs.time)
+//                .append(type, rhs.type)
+//                .append(fcsFile, rhs.fcsFile)
+//                .isEquals();
+//    }
     
     //Module measured
     @Setter

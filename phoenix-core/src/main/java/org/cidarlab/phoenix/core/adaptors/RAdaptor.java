d15cdbeeb1c510bdef9b9153fa0752d88d61cc8c
PrashantVaidyanathan <prash@bu.edu>
2015-12-03 18:32:49 -0500

    can R from command line in phoenix

--- phoenix-core/src/main/java/org/cidarlab/phoenix/core/adaptors/RAdaptor.java
+++ phoenix-core/src/main/java/org/cidarlab/phoenix/core/adaptors/RAdaptor.java
@@ -5,8 +5,16 @@
  */
 package org.cidarlab.phoenix.core.adaptors;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.cidarlab.phoenix.core.controller.Utilities;
 
 /**
  *
@@ -44,6 +52,88 @@ public class RAdaptor {
             }           
         }
         return "";
+    }
+    
+    public static void createRunRScript(String runRFilepath, String keyFilepath, String AnalyzeRFilepath, String dataFilepath, String wd, int minEvents){
+        
+        File runFile = new File(runRFilepath);
+        FileWriter fw = null;
+        BufferedWriter writer = null;
+        try {
+            fw = new FileWriter(runFile);
+            writer = new BufferedWriter(fw);
+            
+            writer.write("#!/usr/bin/Rscript");
+            writer.newLine();
+            writer.write("minEvents <- "+minEvents);
+            writer.newLine();
+            writer.write("superWD <- \""+wd+"\"");
+            writer.newLine();
+            writer.write("wd <- superWD");
+            writer.newLine();
+            writer.write("dataPath <- \""+dataFilepath+"\"");
+            writer.newLine();
+            writer.write("key <- read.csv(\""+keyFilepath+"\", header = TRUE)");
+            writer.newLine();
+            writer.write("source(\""+AnalyzeRFilepath+"\", chdir=T)");
+            writer.close();
+            //Convert file to an executable
+            StringBuilder commandBuilder = new StringBuilder("chmod 755 "+ runRFilepath);       
+            String command = commandBuilder.toString();
+        
+            Runtime runtime = Runtime.getRuntime();
+            Process proc = null;
+            proc = runtime.exec(command);
+            proc.waitFor();
+            
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(RAdaptor.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
     }
+    
+    public static void runR(String filepath){
+        
+        System.out.println("Starting Run R");
+        System.out.println("Filepath :: "+filepath);
+        StringBuilder commandBuilder = null;
+        commandBuilder = new StringBuilder("R CMD BATCH " +filepath);
+        //commandBuilder = new StringBuilder("Rscript "+filepath);
+        //commandBuilder = new StringBuilder("R < " +filepath+ " --no-save");
+        
+        //commandBuilder = new StringBuilder(filepath);
+        String command = commandBuilder.toString();
+        String resourceFilepath = Utilities.getFilepath() + "/src/main/resources/RTest/";
+        Runtime runtime = Runtime.getRuntime();
+        Process proc = null;
+        
+        try {
+            
+            proc = runtime.exec(command);
+            proc.waitFor();
+            InputStream in = proc.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(RAdaptor.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(RAdaptor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        /*try {
+            InputStream in = proc.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(RAdaptor.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
+        
+    }
+    
 }
--- phoenix-core/src/test/java/org/cidarlab/phoenix/adaptors/tests/RAdaptorTest.java
+++ phoenix-core/src/test/java/org/cidarlab/phoenix/adaptors/tests/RAdaptorTest.java
@@ -7,6 +7,7 @@ package org.cidarlab.phoenix.adaptors.tests;
 
 import java.io.IOException;
 import org.cidarlab.phoenix.core.adaptors.RAdaptor;
+import org.cidarlab.phoenix.core.controller.Utilities;
 import org.junit.Test;
 
 /**
@@ -15,11 +16,36 @@ import org.junit.Test;
  */
 public class RAdaptorTest {
     
-    @Test
+    //@Test
     public void getAllDirectories() throws IOException{
         String directory = "/home/prash/cidar/phoenixData/data/degradation/Degradation_102215";
         //String blank = "";
         RAdaptor.getAllData(directory);
+    }
+    
+    //@Test
+    public void runRTest(){
+        System.out.println(Utilities.getFilepath());
+        String resourceFilepath = Utilities.getFilepath() + "/src/main/resources/RTest/";
+        String filepathR = resourceFilepath + "run.R";
+        RAdaptor.runR(filepathR);
+    }
+    
+    @Test
+    public void createRunRTest(){
+        String resourceFilepath = Utilities.getFilepath() + "/src/main/resources/RTest/";
+        String filepathR = resourceFilepath + "run.R";
+        String keyfilepath = resourceFilepath + "key_EXPRESSEE_regulation_degradation_111715_edit.csv";
+        //String keyfilepath = "key_EXPRESSEE_regulation_degradation_111715_edit.csv";
+        //String dataFilepath = resourceFilepath + "data";
+        String dataFilepath = resourceFilepath + "data";
+
+        String cytometryFilepath = Utilities.getCytometryFilepath();
+        
+        cytometryFilepath += "/cytometry_analysis/master_analytics_scripts/analyze.R";
+        RAdaptor.createRunRScript(filepathR, keyfilepath, cytometryFilepath, dataFilepath,resourceFilepath,1000);
+        System.out.println("End of Run Script");
+        RAdaptor.runR(filepathR);
     }
     
 }

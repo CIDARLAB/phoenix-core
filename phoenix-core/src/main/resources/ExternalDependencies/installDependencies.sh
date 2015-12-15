9e204bbe7b7ab3d9a316cc803df2405f7b8b35c3
Curtis Madsen <culnaur@gmail.com>
2015-12-15 18:34:22 -0500

    Added COPASIWS dependency

--- phoenix-core/src/main/resources/ExternalDependencies/installDependencies.sh
+++ phoenix-core/src/main/resources/ExternalDependencies/installDependencies.sh
@@ -3,3 +3,4 @@ mvn install:install-file -Dfile=Datasheet_Generator-1.0-SNAPSHOT.jar -DgroupId=o
 mvn install:install-file -Dfile=clotho3api-jar-with-dependencies.jar -DgroupId=org.clothocad -DartifactId=Clotho3JavaAPI -Dversion=3.0-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=miniEugene-core-1.0.0-jar-with-dependencies.jar -DgroupId=org.cidarlab -DartifactId=miniEugene -Dversion=1.0 -Dpackaging=jar
 mvn install:install-file -Dfile=jsbml-1.0-with-dependencies.jar -DgroupId=org.sbml -DartifactId=jsbml -Dversion=1.0 -Dpackaging=jar
+mvn install:install-file -Dfile=COPASIWS_Services.jar -DgroupId=org.copasi -DartifactId=copasiws -Dversion=1.0 -Dpackaging=jar

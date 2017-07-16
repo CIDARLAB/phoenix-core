d2ff5c98d0f6079139ad0146b9141fe19bd455a0
PrashantVaidyanathan <prash@bu.edu>
2017-07-16 17:56:29 +0100

    basic infrastucture added for Spring framework

---
+++ phoenix-core/src/main/java/org/cidarlab/phoenix/core/visualize/MainController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cidarlab.phoenix.core.visualize;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author prash
+ */
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/getSTL", method = RequestMethod.POST)
+    public void getSTL(HttpServletRequest request, HttpServletResponse response) {
+        
+       
+
+    }
+}
